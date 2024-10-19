de0cf14d2b2459bb2280afbf2565b38ac4c5dca0
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-19 23:33:49 +0530

    Fixed complict with ''use client''

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,18 +1,36 @@
+'use client'
+
+// import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const CountChart = dynamic(() => import("../../../components/CountChart"), {
+  ssr: false,
+});
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* Left */}
-      <div className="w-full lg:w-2/3 bg-red-400">
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
       {/* User Cards */}
-        <div className="flex justify-between">
+        <div className="flex flex-wrap justify-between gap-4">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* Middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+          <CountChart /></div>
+          {/* Attendance chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom chart */}
+        <div className=""></div>
       </div>
 
       {/* Right */}
--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const UserCard = ({ type }: { type: string }) => {
   return (
-    <div className="rounded-2xl odd:bg-PurpleColor even:bg-YellowColor p-4 flex-1 min-w-[130px]">
+    <div className="rounded-2xl odd:bg-PurpleColor even:bg-YellowColor p-4 flex-1 min-w-[130px] shadow-xl">
       <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/25</span>
         <Image src="/more.png" alt="more_icon" width={20} height={20}/>
