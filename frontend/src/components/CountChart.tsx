177bc0bdb87e0396853bb9a49bc3fce6d7f3d597
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-19 23:50:56 +0530

    Student charts updated

--- frontend/src/components/CountChart.tsx
+++ frontend/src/components/CountChart.tsx
@@ -9,46 +9,19 @@ import {
 
 const data = [
   {
-    name: "18-24",
-    uv: 31.47,
-    pv: 2400,
-    fill: "#8884d8",
+    name: "Total",
+    count: 110,
+    fill: "white",
   },
   {
-    name: "25-29",
-    uv: 26.69,
-    pv: 4567,
-    fill: "#83a6ed",
+    name: "Boys",
+    count: 60,
+    fill: "#c3ebfa",
   },
   {
-    name: "30-34",
-    uv: 15.69,
-    pv: 1398,
-    fill: "#8dd1e1",
-  },
-  {
-    name: "35-39",
-    uv: 8.22,
-    pv: 9800,
-    fill: "#82ca9d",
-  },
-  {
-    name: "40-49",
-    uv: 8.63,
-    pv: 3908,
-    fill: "#a4de6c",
-  },
-  {
-    name: "50+",
-    uv: 2.63,
-    pv: 4800,
-    fill: "#d0ed57",
-  },
-  {
-    name: "unknow",
-    uv: 6.67,
-    pv: 4800,
-    fill: "#ffc658",
+    name: "Girls",
+    count: 50,
+    fill: "#fae27c",
   },
 ];
 
@@ -61,29 +34,26 @@ const CountChart = () => {
         <Image src="/moreDark.png" alt="moreIcon" width={20} height={20} />
       </div>
       {/* Chart */}
-      <div className="w-full h-[75%]">
-        <ResponsiveContainer width="100%" height="100%">
+      <div className="relative w-full h-[75%]">
+        <ResponsiveContainer>
           <RadialBarChart
             cx="50%"
             cy="50%"
-            innerRadius="10%"
-            outerRadius="80%"
-            barSize={10}
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={24}
             data={data}
           >
             <RadialBar
-              label={{ position: "insideStart", fill: "#fff" }}
               background
               //clockWise
-              dataKey="uv"
+              dataKey="count"
             />
-            <Legend
-              iconSize={10}
-              layout="vertical"
-              verticalAlign="middle"
-            />
+            
           </RadialBarChart>
         </ResponsiveContainer>
+      <Image src="/maleFemale.png" alt="moreIcon" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+
       </div>
 
       {/* Bottom */}
