9da3bd9ad203f086e1832abe4df640202b1e9786
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-20 13:43:30 +0530

    Implement Announcement component

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/Announcements";
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import EventCalender from "@/components/EventCalendar";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
@@ -8,7 +10,7 @@ import React from "react";
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* Left */}
+      {/*------------- Left -------------*/}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
       {/* User Cards */}
         <div className="flex flex-wrap justify-between gap-4">
@@ -32,8 +34,11 @@ const AdminPage = () => {
         </div>
       </div>
 
-      {/* Right */}
-      <div className="w-full lg:w-1/3 bg-green-400"></div>
+      {/*------------- Right ----------------*/}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8 bg-white rounded-md">
+        <EventCalender/>
+        <Announcements />
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/EventCalendar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { time } from "console";
+import Image from "next/image";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+// temporary data
+const event = [
+  {
+    id: 1,
+    title: "Meeting",
+    time: "10:00 AM - 11:00 AM",
+    description: "Meeting with the team",
+  },
+  {
+    id: 2,
+    title: "Meeting",
+    time: "10:00 AM - 11:00 AM",
+    description: "Meeting with the team",
+  },
+  {
+    id: 3,
+    title: "Meeting",
+    time: "10:00 AM - 11:00 AM",
+    description: "Meeting with the team",
+  },
+];
+
+const EventCalender = () => {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div className="bg-white rounded-md shadow-lg px-4">
+      <Calendar onChange={onChange} value={value} />
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-semibold my-4">Events</h1>
+          <Image src="/moreDark.png" alt="moreicon" width={20} height={20} />
+        </div>
+        {event.map((event) => (
+          <div key={event.id} className="p-5 rounded-md border-gray-100 border-t-4 odd:border-t-skyColor even:border-t-PurpleColor">
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <p className="text-xs text-gray-400">{event.time}</p>
+            </div>
+            <p className="text-sm">{event.description}</p>
+          </div>
+        ))}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCalender;
