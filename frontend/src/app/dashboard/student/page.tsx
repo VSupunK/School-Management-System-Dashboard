a6ca3b3f9b14ef42df58fd78a47cfc662cc5c3b6
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-21 22:59:02 +0530

    Implement BigCalender component

--- frontend/src/app/dashboard/student/page.tsx
+++ frontend/src/app/dashboard/student/page.tsx
@@ -1,8 +1,25 @@
+import EventCalender from '@/components/EventCalendar'
+import Announcements from "@/components/Announcements";
 import React from 'react'
+import BigCalendar from '@/components/BigCalendar';
 
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row md:flex-row'>
+      {/*------------- Left -------------*/}
+      <div className="w-full xl:w-2/3 flex flex-col gap-8">
+      <div className="h-full bg-white p-4 rounded-md">
+        <h1 className='text-xl font-semibold'>Schedule (4-A)</h1>
+        <BigCalendar />
+      </div>
+      </div>
+
+      {/*------------- Right ----------------*/}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8 bg-white rounded-md">
+        <EventCalender/>
+        <Announcements />
+      </div>
+    </div>
   )
 }
 
