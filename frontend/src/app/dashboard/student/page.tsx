8d27c8b417ec7df82b5e044f2fdb9addec633625
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-22 23:07:04 +0530

    updated global.css

--- frontend/src/app/dashboard/student/page.tsx
+++ frontend/src/app/dashboard/student/page.tsx
@@ -1,13 +1,12 @@
 import EventCalender from '@/components/EventCalendar'
 import Announcements from "@/components/Announcements";
-import React from 'react'
 import BigCalendar from '@/components/BigCalendar';
 
 const StudentPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col xl:flex-row md:flex-row'>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
       {/*------------- Left -------------*/}
-      <div className="w-full xl:w-2/3 flex flex-col gap-8">
+      <div className="w-full xl:w-2/3">
       <div className="h-full bg-white p-4 rounded-md">
         <h1 className='text-xl font-semibold'>Schedule (4-A)</h1>
         <BigCalendar />
