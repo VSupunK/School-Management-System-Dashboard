8d8527c6a9b3bea4906a169d7933a051983bd719
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-30 18:21:28 +0530

    Updated Teacher List

--- frontend/src/app/dashboard/teacher/page.tsx
+++ frontend/src/app/dashboard/teacher/page.tsx
@@ -1,65 +1,21 @@
-import Pagination from "@/components/Pagination";
-import Table from "@/components/Table";
-import TableSearch from "@/components/TableSearch";
-import Image from "next/image";
-import React from "react";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
 
-const columns = [
-  {
-    header:'Info', accessor:'info'
-  },{
-    header:'Teacher ID',
-    accessor: 'teacherId',
-    className: 'hidden md:table-cell'
-  },{
-    header: 'Subjects',
-    accessor: 'subjects',
-    className: 'hidden md:table-cell'
-  },
-  {
-    header: 'Classes',
-    accessor: 'classes',
-    className: 'hidden md:table-cell'
-  },{
-    header: 'Phone',
-    accessor: 'phone',
-    className: 'hidden lg:table-cell'
-  },{
-    header: 'Address',
-    accessor: 'address',
-    className: 'hidden lg:table-cell'
-  },{
-    header: 'Actions',
-    accessor: 'actions',
-    className: 'hidden lg:table-cell'
-  }
-]
 
 const TeacherPage = () => {
   return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* Top */}
-      <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-          <TableSearch />
-          <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex justify-center items-center rounded-full bg-YellowColor">
-              <Image src="/filter.png" alt="" width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex justify-center items-center rounded-full bg-YellowColor">
-              <Image src="/sort.png" alt="" width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex justify-center items-center rounded-full bg-YellowColor">
-              <Image src="/plus.png" alt="" width={14} height={14} />
-            </button>
-          </div>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
         </div>
       </div>
-      {/* List */}
-      <Table columns={columns}/>
-      {/* Pagination */}
-      <Pagination />
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
     </div>
   );
 };
--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-const Table = (
-    {columns}: { columns: { header: string; accessor: string; className?: string | undefined; }[];
+const Table = ({
+  columns,
+  renderRow,
+  data
+}: {
+  columns: {
+    header: string;
+    accessor: string;
+    className?: string | undefined;
+  }[];
+  renderRow: (item:any) => React.ReactNode;
+  data: any[];
 }) => {
   return (
     <table className="w-full mt-4">
       <thead>
         <tr className="text-left text-gray-500 text-sm">
-            {columns.map((col)=>(
-                <th key={col.accessor}>{col.header}</th>
-            ))}
+          {columns.map((col) => (
+            <th key={col.accessor}>{col.header}</th>
+          ))}
         </tr>
       </thead>
+      <tbody>{data.map((item)=> renderRow(item))}</tbody>
     </table>
   );
 };
 
 export default Table;
- 
