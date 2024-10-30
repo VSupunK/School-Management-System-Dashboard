c9bbac7837eac5cb5eae45d17eaff60091e1f50d
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-30 20:16:24 +0530

    Updated Teachers list

--- frontend/src/app/dashboard/list/teachers/page.tsx
+++ frontend/src/app/dashboard/list/teachers/page.tsx
@@ -52,9 +52,9 @@ const columns = [
 const TeacherListPage = () => {
 
   const renderRow = (item: Teacher) => (
-    <tr key={item.id}>
-      <td>
-        <Image src={item.photo} alt="teacherPhoto" width={40} height={40} className="md:hidden xl:block w-10 rounded-full object-cover"/>
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-PurpleLightColor">
+      <td className="flex items-center gap-4 p-4">
+        <Image src={item.photo} alt="teacherPhoto" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"/>
       
       <div className="flex flex-col">
         <h3 className="font-semibold">{item.name}</h3>
--- frontend/src/app/dashboard/parent/page.tsx
+++ frontend/src/app/dashboard/parent/page.tsx
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const ParentPage = () => {
   return (
-    <div>ParentPage</div>
+    <div>
+      <Image src="https://images.pexels.com/photos/1438081/pexels-photo-1438081.jpeg?auto=compress&cs=tinysrgb&w=1200" alt='' width={50} height={50}/>
+    </div>
   )
 }
 
