643023dc378785aba645aab6e2963965986e781e
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-01 23:21:11 +0530

    updated

--- frontend/src/app/dashboard/list/teachers/[id]/page.tsx
+++ frontend/src/app/dashboard/list/teachers/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const SigleTeacherPage = () => {
   return (
-    <div className="flex p-4 flex-col gap-4 xl:flex-row">
+    <div className="flex-1 p-4 flex-col gap-4 xl:flex-row">
       {/* Left */}
       <div className="w-full xl:w-2/3">
         {/* Top */}
@@ -20,10 +20,12 @@ const SigleTeacherPage = () => {
               />
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
+            <div className="flex items-center gap-4">
               <h1 className="text-lg font-semibold">John Smith</h1>
               <p className="text-sm text-gray-500">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               </p>
+              </div>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 2xl:h-1/3 flex items-center gap-2">
                   <Image src="/blood.png" alt="" width={14} height={14} />
@@ -38,7 +40,7 @@ const SigleTeacherPage = () => {
                   <Image src="/mail.png" alt="" width={14} height={14} />
                 </div>
                 <div className="w-full md:w-1/3 flex items-center gap-2">
-                  <Image src="/" alt="" width={14} height={14} />
+                  <Image src="/phone.png" alt="" width={14} height={14} />
                   <span>+84 012 0365 42</span>
                 </div>
               </div>
