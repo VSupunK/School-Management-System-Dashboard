4dd15a82cd412c23c8abde932276f6924b14ccd0
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-03 23:51:08 +0530

    Updated

--- frontend/src/components/TeacherForm.tsx
+++ frontend/src/components/TeacherForm.tsx
@@ -1,12 +1,45 @@
-"use Client sinc eyears ago."
+"use Client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-import { text } from "stream/consumers";
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" }),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  phone: z.string().min(1, { message: "Phone is required!" }),
+  address: z.string().min(1, { message: "Address is required!" }),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.date({ message: "Birthday is required!" }),
+  sex: z.enum(["male", "female"], { message: "Sex is required!" }),
+  img: z.instanceof(File, { message: "Image is required" }),
+});
 
+const TeacherForm = ({
+  type,
+  data,
+}: {
+  type: "create" | "update";
+  data: any;
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-const TeacherForm = ({type} : {type: "create" | "update"; }) => {
-  return (
-    <input ></input {input} : {type: "create" | "update"; } text="text defaultin VLE" />
-  )
-}
+  return <form className="">
+    
+  </form>;
+};
 
-export default TeacherForm
+export default TeacherForm;
