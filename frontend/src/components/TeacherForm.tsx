9c01c4d87e6ae01ede1b1d9bf572d43b9a2efc61
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-03 22:59:38 +0530

    Add TeacherForm component for creating and updating teacher records

---
+++ frontend/src/components/TeacherForm.tsx
@@ -0,0 +1,12 @@
+"use Client sinc eyears ago."
+
+import { text } from "stream/consumers";
+
+
+const TeacherForm = ({type} : {type: "create" | "update"; }) => {
+  return (
+    <input ></input {input} : {type: "create" | "update"; } text="text defaultin VLE" />
+  )
+}
+
+export default TeacherForm
