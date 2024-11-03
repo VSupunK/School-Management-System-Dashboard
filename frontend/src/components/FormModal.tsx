b5ad1fc167353624f8c3589f8ff56a3c74cad40f
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-03 22:32:40 +0530

    Integrate FormModal functionality; add state management for modal visibility and close button in FormModal component

--- frontend/src/components/FormModal.tsx
+++ frontend/src/components/FormModal.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const FormModal = ({
   table,
@@ -34,13 +34,24 @@ const FormModal = ({
       ? "bg-skyColor"
       : "bg-PurpleColor";
 
+  const [open, setOpen] = useState(false);
   return (
     <>
       <button
         className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
       >
-        <Image src={`/${type}.png`} alt="" width={16} height={16}/>
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:[50%] 2xl:[40%]">
+            <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
+              <Image src='/close.png' alt='closeIcon' width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
