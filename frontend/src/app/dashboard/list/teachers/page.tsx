4f31eae66b6da99e7fbf714e0036c2a0bc3780a4
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-03 09:50:25 +0530

    Refactor FormModal integration for teacher list; update delete functionality for students and create functionality for teachers

--- frontend/src/app/dashboard/list/teachers/page.tsx
+++ frontend/src/app/dashboard/list/teachers/page.tsx
@@ -87,10 +87,12 @@ const TeacherListPage = () => {
               <Image src="/view.png" alt="" width={16} height={16} />
             </button>
           </Link>
-          {role === "admin" && (
+          {/* {role === "admin" && (
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-PurpleColor">
               <Image src="/delete.png" alt="" width={16} height={16} />
             </button>
+          )} */}
+           {role === "admin" && (<FormModal table="student" type="delete" id={item.id}/>
           )}
         </div>
       </td>
@@ -115,7 +117,7 @@ const TeacherListPage = () => {
               // <button className="w-8 h-8 flex justify-center items-center rounded-full bg-YellowColor">
               //   <Image src="/plus.png" alt="" width={14} height={14} />
               // </button>
-              <FormModal table="teacher" type="delete" id={item.id}/>
+              <FormModal table="student" type="create"/>
             )}
           </div>
         </div>
