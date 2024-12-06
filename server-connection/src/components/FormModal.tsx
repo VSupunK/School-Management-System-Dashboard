30d03a8e815945df9ddc684b8cc89c4eee55b4d9
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-12-06 15:21:13 +0530

    Refactor StudentListPage to support dynamic filtering and pagination with Prisma, and update FormModal to handle string IDs

--- server-connection/src/components/FormModal.tsx
+++ server-connection/src/components/FormModal.tsx
@@ -14,11 +14,12 @@ const StudentForm = dynamic(() => import("./forms/StudentForm"), {
   loading: () => <h1>Loading...</h1>,
 });
 
-const forms:{[key: string]: (type: "create" | "update", data?:any) => JSX.Element;
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
 } = {
   teacher: (type, data) => <TeacherForm type={type} data={data} />,
   student: (type, data) => <StudentForm type={type} data={data} />,
-}
+};
 
 const FormModal = ({
   table,
@@ -41,7 +42,7 @@ const FormModal = ({
     | "announcement";
   type: "create" | "update" | "delete";
   data?: any;
-  id?: number;
+  id?: number | string;
 }) => {
   const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
   const bgColor =
@@ -56,13 +57,19 @@ const FormModal = ({
   const Form = () => {
     return type === "delete" && id ? (
       <form action="" className="p-4 flex flex-col gap-4">
-        <span className="text-center font-medium">All data will be lost!!! Are sure, you want to delete this. </span>
-        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
+        <span className="text-center font-medium">
+          All data will be lost!!! Are sure, you want to delete this.{" "}
+        </span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+          Delete
+        </button>
       </form>
     ) : type === "create" || type === "update" ? (
       forms[table](type, data)
-    ) : ("Form not found!!!");
-  }
+    ) : (
+      "Form not found!!!"
+    );
+  };
 
   return (
     <>
@@ -76,8 +83,11 @@ const FormModal = ({
         <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:[50%] 2xl:[40%]">
             <Form />
-            <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
-              <Image src='/close.png' alt='closeIcon' width={14} height={14} />
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="closeIcon" width={14} height={14} />
             </div>
           </div>
         </div>
