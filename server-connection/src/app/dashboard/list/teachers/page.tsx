da19ec6c2578e6aa7d9300fcf1812ae52b8db153
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-12-06 15:00:47 +0530

    Add search functionality to TeacherListPage for case-insensitive name filtering

--- server-connection/src/app/dashboard/list/teachers/page.tsx
+++ server-connection/src/app/dashboard/list/teachers/page.tsx
@@ -112,6 +112,9 @@ const TeacherListPage = async ({
                 classId: parseInt(value),
               },
             };
+            break;
+          case "search":
+            query.name = { contains: value, mode: "insensitive" };
         }
       }
     }
