7d4bdb27e45b1b554c9163e536084189ccc59f3f
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-12-12 10:20:45 +0530

    Refactor Search filter

--- server-connection/src/app/dashboard/list/lessons/page.tsx
+++ server-connection/src/app/dashboard/list/lessons/page.tsx
@@ -97,7 +97,10 @@ const LessonListPage = async ({
             query.teacherId = value;
             break;
           case "search":
-            query.name = { contains: value, mode: "insensitive" };
+            query.OR = [
+              { subject: { name: { contains: value, mode: "insensitive" } } },
+              { teacher: { name: { contains: value, mode: "insensitive" } } },
+            ];
         }
       }
     }
