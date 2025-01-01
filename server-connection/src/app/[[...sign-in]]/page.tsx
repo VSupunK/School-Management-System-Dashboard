cd198e28839eb08c8a5d459bc72627b686031488
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2025-01-01 11:04:02 +0530

    Implement custom login page using Clerk elements for enhanced user experience

--- server-connection/src/app/[[...sign-in]]/page.tsx
+++ server-connection/src/app/[[...sign-in]]/page.tsx
@@ -1,5 +1,20 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
 
-export default function Page() {
-  return <SignIn />;
-}
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+
+const LoginPage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-skyLightColor">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
+        >
+          <h1>School Managements System</h1>
+          <h2>Sign in to your account...</h2>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+};
